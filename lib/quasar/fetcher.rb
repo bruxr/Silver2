f83ce99a24a31dffeeae3635cd064f38906bcee7
Brux <brux.romuar@gmail.com>
2014-08-20 17:26:10 +0800

    add the body to get method

--- lib/quasar/fetcher.rb
+++ lib/quasar/fetcher.rb
@@ -3,7 +3,9 @@ class Quasar::Fetcher
   # Sends a GET request to a URL
   def get(url, data = {}, headers = {})
 
-
+    url << data.to_query
+    http = Curl.get url
+    return http.body_str
 
   end
 
