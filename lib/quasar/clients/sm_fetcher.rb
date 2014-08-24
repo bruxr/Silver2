7536247912aa26baa9354868952465bbc745f5d3
Brux <brux.romuar@gmail.com>
2014-08-24 15:03:57 +0800

    move fetchers to clients module

---
+++ lib/quasar/clients/sm_fetcher.rb
@@ -0,0 +1,85 @@
+module Quasar
+  module Clients
+
+    # The base class of all Quasar Fetchers for SM Cinemas.
+    # Provides common methods for accessing the
+    # cinemas' AJAX endpoints, automatically converting
+    # responses to JSON.
+    # 
+    # TODO: find a way on how to buy tickets b/c sm requires
+    # a popup login to buy tickets.
+    class SmFetcher < Fetcher
+
+      # Returns an array of screening times for
+      # movies available to a branch
+      def get_schedules()
+
+        @schedules = []
+
+        movies = get_movies @branch_code
+        movies.each do |item|
+          
+          movie = {}
+          movie[:name] = item['movie_name']
+          movie[:schedules] = []
+
+          screenings = get_screenings @branch_code, movie[:name]
+          
+          screenings.each do |cinema, schedules|
+            schedules.each do |screening|
+              sked = {}
+              sked[:cinema_name] = screening['CinemaName']
+              sked[:price] = screening['Price'].to_f
+              sked[:time] = Time.zone.parse screening['StartTime']
+              if screening['FilmFormat'] == 'F2D'
+                sked[:format] = '2D'
+              elsif screening['FilmFormat'] == 'F3D'
+                sked[:format] = '3D'
+              end
+              sked[:ticket_url] = "http://smcinema.com/movies/buy-tickets/?mctkey=#{screening['MctKey']}"
+              movie[:rating] = screening['MtrcbRating']
+              movie[:schedules] << sked
+            end
+          end
+
+          @schedules << movie
+
+        end
+
+        return @schedules
+
+      end
+
+      protected
+
+        # Returns the movies available under a branch
+        # (e.g. SMCD for SM City Davao)
+        def get_movies(branch_code)
+
+          data = {
+            method: 'listMovies',
+            branch_code: branch_code
+          }
+          resp = post 'http://smcinema.com/ajaxMovies.php', data
+          JSON.parse resp
+
+        end
+
+        # Returns the screening times for a movie.
+        # Provide the branch code & movie name from get_movies().
+        def get_screenings(branch_code, movie_name)
+
+          data = {
+            method: 'listScreeningTime',
+            branch_code: branch_code,
+            movie_name: movie_name
+          }
+          resp = post 'http://smcinema.com/ajaxMovies.php', data
+          JSON.parse resp
+
+        end
+
+    end
+
+  end
+end
---
+++ lib/quasar/clients/sm_lanang.rb
@@ -0,0 +1,21 @@
+module Quasar
+  module Clients
+
+    # A Quasar Fetcher for SM Lanang Premiere Cinemas
+    # Instantiate and invoke get_schedules() to 
+    # return an array of screening times.
+    # 
+    # Take note that SM only publishes screening times
+    # after the current date & time. Be sure to 
+    # fetch them before the start of each day!
+    class SmLanang < SmFetcher
+
+      def initialize
+        super
+        @branch_code = 'SMLA'
+      end
+
+    end
+
+  end
+end
