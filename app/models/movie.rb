1eadb8738e73c3525f680da036494e29f56267b9
Brux <brux.romuar@gmail.com>
2014-08-25 13:14:36 +0800

    validate mtrcb_rating if it is present

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,5 +3,12 @@ class Movie < ActiveRecord::Base
 
   has_many :cinemas, through: :schedules
 
+  validates :mtrcb_rating, inclusion: {
+    in: %w(G PG-13 R-13 R-16 R-18),
+    message: "%{value} is not a valid MTRCB Rating.",
+    allow_nil: true
+  }
+
   slugify :title
+
 end
