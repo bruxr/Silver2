43528609535a9e4f22ffce16e71dffede3fbdf2e
Brux <brux.romuar@gmail.com>
2014-08-25 14:44:13 +0800

    make sure we have movie titles :)

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,8 @@ class Movie < ActiveRecord::Base
 
   has_many :schedules, inverse_of: :movie, dependent: :destroy
 
+  validates :title, presence: true
+
   validates :mtrcb_rating, inclusion: {
     in: %w(G PG R-13 R-16 R-18),
     message: "%{value} is not a valid MTRCB Rating.",
