329ab1d9d5fba94e83381ed1daab8e8c75c84c81
Brux <brux.romuar@gmail.com>
2014-08-25 13:33:18 +0800

    fix mtrcb rating PG-13 to just PG

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,7 +4,7 @@ class Movie < ActiveRecord::Base
   has_many :cinemas, through: :schedules
 
   validates :mtrcb_rating, inclusion: {
-    in: %w(G PG-13 R-13 R-16 R-18),
+    in: %w(G PG R-13 R-16 R-18),
     message: "%{value} is not a valid MTRCB Rating.",
     allow_nil: true
   }
