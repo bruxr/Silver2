fb19fd9a57a02e9fb253674646a0c68a750d10eb
Brux <brux.romuar@gmail.com>
2014-08-25 14:48:26 +0800

    add validation rules to cinemas

--- app/models/cinema.rb
+++ app/models/cinema.rb
@@ -3,6 +3,18 @@ class Cinema < ActiveRecord::Base
 
   has_many :schedules, inverse_of: :cinema, dependent: :destroy
 
+  validates :name, presence: true
+
+  # Makes sures that a cinema's status is
+  # only of the specified types:
+  # active - active & open cinema
+  # hidden - active & open but not shown to visitors
+  # disabled - closed cinema, no syncs or scraping.
+  validates :status, inclusion: {
+    in: %w(active inactive hidden disabled),
+    message: "%{value} can only be active, inactive, hidden or disabled."
+  }
+
   slugify :name
 
 end
