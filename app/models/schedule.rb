49a4c4dabf82aececc88b13a5670164568a8bbae
Brux <brux.romuar@gmail.com>
2014-08-25 14:34:29 +0800

    add validations to schedules

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,6 +1,26 @@
 class Schedule < ActiveRecord::Base
 
   belongs_to :cinema, inverse_of: :schedules
+  validates :cinema, presence: true
+
   belongs_to :movie, inverse_of: :schedules
+  validates :movie, presence: true
+
+  validates :format, inclusion: {
+    in: %w(2D 3D IMAX)
+    message: "%{value} is not a valid film format."
+  }
+
+  validates :ticket_url, format: {
+    with: /\Ahttps?\:\/\/[^\n]+\z/i,
+    allow_nil: true,
+    message: "is not a valid URL."
+  }
+
+  validates :price, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0,
+    allow_nil: true
+  }
 
 end
