b6739fe327c48dd3c1888eb1ae355c2178c83a5d
Brux <brux.romuar@gmail.com>
2014-09-17 07:33:04 +0800

    added error classes

---
+++ lib/quasar/exceptions.rb
@@ -0,0 +1,28 @@
+module Quasar
+  module Exceptions
+
+    # Generic class for errors when accessing websites.
+    # Pass in a HTTParty::Response object if it's available:
+    # raise SiteError.new(response), "Failed to access website!"
+    class SiteError < IOError
+      attr_reader :response
+
+      def initialize(response = nil)
+        @response = response unless response.nil?
+      end
+    end
+
+    # Raised when a HTTP request received a page that 
+    # it doesn't expect. (e.g. got redirected to another page)
+    # Pass in the URL when raising the error:
+    # raise UnexpectedUrl.new(url), "Got redirected to the login page!"
+    class UnexpectedUrl < SiteError
+      attr_reader :url
+
+      def initialize(url)
+        @url = url
+      end
+    end
+
+  end
+end
