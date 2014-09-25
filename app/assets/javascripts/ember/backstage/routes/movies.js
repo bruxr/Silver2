5eafdc8db7f7f1d24a72008ccf6d3d479992beef
Brux <brux.romuar@gmail.com>
2014-09-25 17:19:02 +0800

    redirect index to movies route

--- app/assets/javascripts/ember/backstage/routes/movies.js
+++ app/assets/javascripts/ember/backstage/routes/movies.js
@@ -1,3 +1,9 @@
+Silver.IndexRoute = Ember.Route.extend({
+  redirect: function() {
+    this.transitionTo('movies')
+  }
+});
+
 Silver.MoviesRoute = Ember.Route.extend({
   model: function() {
     return this.store.findAll('movie');
