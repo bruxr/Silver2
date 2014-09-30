397a9976dedd83d97c4e0fb36089bf725f613735
Brux <brux.romuar@gmail.com>
2014-09-30 17:03:41 +0800

    add test for schedule model

--- test/models/schedule_test.rb
+++ test/models/schedule_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ScheduleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'old schedules scope' do
+    sked = Schedule.old.first
+    assert_equal('IMAX', sked.format)
+  end
+
+  test 'archiving old records' do
+    count = Schedule.archive_old_records
+    assert_equal(1, count)
+  end
+
 end
