5479e75c670118391d3a3fcb6305a3fe462536d2
Brux <brux.romuar@gmail.com>
2014-10-01 16:52:45 +0800

    enable sentry error tracking

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,6 +1,5 @@
-# TODO: Reenable error tracking!
-#require 'raven'
+require 'raven'
 
-#Raven.configure do |config|
-#  config.dsn = 'https://56bfb01fa69c4d4faa35cd14d629ee97:93b5c71ff0cc49529e4d0ee555f80b9e@app.getsentry.com/30803'
-#end
+Raven.configure do |config|
+  config.dsn = 'https://56bfb01fa69c4d4faa35cd14d629ee97:93b5c71ff0cc49529e4d0ee555f80b9e@app.getsentry.com/30803'
+end
