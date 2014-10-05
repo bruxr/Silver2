b5e56109ba2912011342c2d056d35bad3ee82499
Brux <brux.romuar@gmail.com>
2014-10-05 15:40:52 +0800

    preload sanitize gem so it's ready for use

--- app/scrapers/scraper.rb
+++ app/scrapers/scraper.rb
@@ -1,6 +1,9 @@
 # The base class for all Cinema scrapers.
 # Functions as an interface that defines what methods
 # a scraper should have.
+#
+# The sanitize gem is available so you can use it to
+# clean up scraped information.
 class Scraper < WebClient
 
   attr_reader :schedules
@@ -8,6 +11,7 @@ class Scraper < WebClient
   # Setups an empty array as schedules
   def initialize
     @schedules = []
+    require 'sanitize'
   end
 
   # Subclasses should override this method
