bc37e1ede054fdd8f1bd1528a413c43bd1e5f89e
Brux <brux.romuar@gmail.com>
2014-10-05 12:22:23 +0800

    updating a single movie job is not recurring

--- app/jobs/update_single_movie_scores_job.rb
+++ app/jobs/update_single_movie_scores_job.rb
@@ -1,9 +1,7 @@
 # Updates a movie's scores/ratings from its external sources.
 class UpdateSingleMovieScoresJob
   include Sidekiq::Worker
-  include Sidetiq::Schedulable
 
-  recurrence { daily.hour_of_day(6, 12, 18, 24) }
   sidekiq_options throttle: { threshold: 300, period: 1.hour, key: 'rt-api-client' }
 
   def perform(movie_id)
