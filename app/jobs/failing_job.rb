fe2b676be6d00374a8cda276f6be98f3b68ae036
Brux <brux.romuar@gmail.com>
2014-10-13 17:19:32 +0800

    make test jobs run every hour

--- app/jobs/failing_job.rb
+++ app/jobs/failing_job.rb
@@ -2,7 +2,7 @@ class FailingJob
   include Sidekiq::Worker
   extend Scheduler::Schedulable
 
-  every 1.minute
+  every 1.hour
 
   def perform
     raise "I just fail at doing jobs :("
--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -2,7 +2,7 @@ class TestJob
   include Sidekiq::Worker
   extend Scheduler::Schedulable
 
-  every 1.minute
+  every 1.hour
 
   def perform
     Rails.logger.info("hello world!")
