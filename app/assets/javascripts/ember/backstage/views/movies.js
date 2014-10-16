9874478f05211ac821a5b3f8aeb918863ba97b67
Brux <brux.romuar@gmail.com>
2014-10-16 17:29:45 +0800

    remove fade in images effect since filtering broke it

--- app/assets/javascripts/ember/backstage/views/movies.js
+++ app/assets/javascripts/ember/backstage/views/movies.js
@@ -1,8 +1,3 @@
 Silver.MovieGridView = Ember.View.create({
-  templateName: 'movie-grid',
-  didInsertElement: function() {
-    $('img.fi', this.$()).each(function(i, el) {
-      $(el).on('load', function() { this.className = this.className + ' fi-loaded'; })
-    });
-  }
+  templateName: 'movie-grid'
 });
