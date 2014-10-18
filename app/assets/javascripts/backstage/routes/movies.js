60c95015cef531370eb77d66cb24be109c00cdcb
Brux <brux.romuar@gmail.com>
2014-10-18 15:02:59 +0800

    add a left side movie filter

--- app/assets/javascripts/backstage/routes/movies.js
+++ app/assets/javascripts/backstage/routes/movies.js
@@ -1,5 +1,10 @@
 Backstage.MoviesRoute = Ember.Route.extend({
   model: function(params) {
   	return this.store.findAll('movie')
+  },
+  renderTemplate: function() {
+    this.render('movies-filter', {
+      outlet: 'toolbars'
+    });
   }
 });
