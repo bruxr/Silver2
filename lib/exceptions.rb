18ab99f4ff91c29537d8f8ccf6c5d4e4d4b807fc
Brux <brux.romuar@gmail.com>
2014-10-22 16:35:20 +0800

    fix missing end

--- lib/exceptions.rb
+++ lib/exceptions.rb
@@ -2,6 +2,7 @@ module Exceptions
   
   # Raised whenever a deprecated method is invoked
   class DeprecatedMethod < StandardError
+  end
   
   # Raised when we reach a web service's quota.
   class QuotaReached < StandardError
