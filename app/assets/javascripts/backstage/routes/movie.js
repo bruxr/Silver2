03572491127dd7118ded58bfa209bc352cc5ecee
Brux <brux.romuar@gmail.com>
2014-10-23 17:33:38 +0800

    improved movie section

--- app/assets/javascripts/backstage/routes/movie.js
+++ app/assets/javascripts/backstage/routes/movie.js
@@ -1,5 +1,11 @@
 Backstage.MovieRoute = Ember.Route.extend({
   model: function(params) {
   	return this.store.find('movie', params.id);
+  },
+  actions: {
+    didTransition: function() {
+      $('.movie-popover').addClass('shown');
+      $('.curtains').addClass('shown');
+    }
   }
 });
