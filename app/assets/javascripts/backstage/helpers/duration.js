0409cbc5297d0fe7b29f7a64959d43afd69a07e8
brux <imoz39@gmail.com>
2014-10-29 18:56:03 +0800

    add duration helper for friendly runtimes

---
+++ app/assets/javascripts/backstage/helpers/duration.js
@@ -0,0 +1,14 @@
+Ember.Handlebars.helper('duration', function(value, options) {
+  value = parseInt(value);
+  if (value <= 0 || isNaN(value)) {
+    return new Ember.Handlebars.SafeString('? mins');
+  }
+  else {
+    var hours = Math.ceil(value / 60),
+        mins = value % 60,
+        str = '';
+    if (hours > 0) { str += "%d hrs".replace('%d', hours); }
+    str += " %d mins".replace('%d', mins);
+    return new Ember.Handlebars.SafeString(str);
+  }
+});
