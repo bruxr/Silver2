bb21ffb8ea3cea76f7ba8c27ce95f298974a1012
brux <imoz39@gmail.com>
2014-10-31 18:27:46 +0800

    remove unused trailer url

--- app/assets/javascripts/backstage/controller/movie.js
+++ app/assets/javascripts/backstage/controller/movie.js
@@ -1,6 +1,2 @@
 Backstage.MovieController = Ember.ObjectController.extend({
-  trailerUrl: function() {
-    var trailer = this.get('model.trailer');
-    return 'http://www.youtube.com/embed/%s?controls=0&amp;disablekb=1&amp;iv_load_policy=3&amp;modestbranding=1&amp;rel=0&amp;showinfo=0'.replace('%s', trailer);
-  }.property('model.trailer')
 });
