5a28089fff09305f06b188147e0307e77d659aff
brux <imoz39@gmail.com>
2014-10-31 10:39:12 +0800

    use activemodel serializer

--- app/assets/javascripts/backstage/serializers/movie.js
+++ app/assets/javascripts/backstage/serializers/movie.js
@@ -1,4 +1,4 @@
-Backstage.MovieSerializer = DS.RESTSerializer.extend(DS.EmbeddedRecordsMixin, {
+Backstage.MovieSerializer = DS.ActiveModelSerializer.extend(DS.EmbeddedRecordsMixin, {
   attrs: {
     sources: { embedded: 'always' },
     schedules: { embedded: 'always' }
