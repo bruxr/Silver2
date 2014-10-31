e06c275d67017d0c273a9548d4df7747ae1a96fd
brux <imoz39@gmail.com>
2014-10-31 11:49:41 +0800

    fix derped data types again

--- app/assets/javascripts/backstage/models/cinema.js
+++ app/assets/javascripts/backstage/models/cinema.js
@@ -1,10 +1,10 @@
 Backstage.Cinema = DS.Model.extend({
   name:       DS.attr('string'),
-  slug:       DS.attr('slug'),
+  slug:       DS.attr('string'),
   latitude:   DS.attr('number'),
   longitude:  DS.attr('number'),
   status:     DS.attr('string'),
-  fetcher:    DS.attr('fetcher'),
+  fetcher:    DS.attr('string'),
   createdAt:  DS.attr('date'),
   updatedAt:  DS.attr('date')
 })
