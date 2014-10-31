353755e48ecb605037f7e7b0b967098df7f9ea02
brux <imoz39@gmail.com>
2014-10-31 19:49:30 +0800

    add basic movie editing form

--- app/assets/javascripts/backstage/controller/movie.js
+++ app/assets/javascripts/backstage/controller/movie.js
@@ -1,2 +1,19 @@
 Backstage.MovieController = Ember.ObjectController.extend({
+  isEditing: false,
+  mtrcbRatings: ['G', 'PG', 'R-13', 'R-16', 'R-18'],
+  trailerUrl: function() {
+    return 'http://www.youtube.com/watch?v='+ this.get('trailer');
+  }.property('trailer'),
+  
+  actions: {
+    edit: function() {
+      this.set('isEditing', true);
+    },
+    doneEditing: function() {
+      this.get('model').save().then(function() {
+        this.set('isEditing', false);
+      });
+    }
+  }
+  
 });
