3521ed3afb48be3f814920f2ab0c5e6c0faba94e
brux <imoz39@gmail.com>
2014-10-31 12:07:41 +0800

    customize moment calendar time

---
+++ app/assets/javascripts/backstage/moment-mods.js
@@ -0,0 +1,10 @@
+moment.locale('en', {
+  calendar: {
+    lastDay: '[Yesterday] LT',
+    sameDay: '[Today] LT',
+    nextDay: '[Tomorrow] LT',
+    lastWeek: 'lll',
+    nextWeek: 'lll',
+    sameElse: 'lll'
+  }
+});
