e19bafd9692595fbdc166a51eef44f55f6887739
brux <imoz39@gmail.com>
2014-10-31 10:22:00 +0800

    we don't need the external id

--- app/assets/javascripts/backstage/models/source.js
+++ app/assets/javascripts/backstage/models/source.js
@@ -1,7 +1,6 @@
 Backstage.Source = DS.Model.extend({
   movie:        DS.belongsTo('movie'),
   name:         DS.attr('string'),
-  external_id:  DS.attr('string'),
   url:          DS.attr('string'),
   score:        DS.attr('number')
 })
