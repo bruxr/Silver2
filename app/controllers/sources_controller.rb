a56ab403b32078f689cf3666d57008e92b8ae9e5
brux <imoz39@gmail.com>
2014-11-02 15:39:02 +0800

    derp double controller word

---
+++ app/controllers/sources_controller.rb
@@ -0,0 +1,40 @@
+# Controller for the /api/sources resource.
+class SourcesControllerController < ApplicationController
+  
+  before_action :set_source, only: [:update, :destroy]
+  
+  # PATCH/PUT /sources/<id>.json route
+  # Updates a source's info.
+  def update
+    respond_to do |format|
+      if @source.update(source_params)
+        format.json { render :show, status: :ok, location: @source }
+      else
+        format.json { render json: @source.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  # DELETE /sources/<id>.json route
+  # Deletes a source.
+  def destroy
+    movie_id = @source.movie_id
+    @source.destroy
+    Movie.find(movie_id).update_scores!
+    
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+  
+  private
+  
+    def set_source
+      @source = Source.find(params[:id])
+    end
+    
+    def source_params
+      params.require(:source)
+    end
+  
+end
--- app/controllers/sources_controller_controller.rb
+++
@@ -1,6 +0,0 @@
-# Controller for the /api/sources resource.
-class SourcesControllerController < ApplicationController
-  
-  before_action :set_source, only: [:update, :source]
-  
-end
