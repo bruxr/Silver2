9951f33092a697259ca1259c93ef70f48fefb314
brux <imoz39@gmail.com>
2014-11-08 15:52:53 +0800

    stylized cinema view

--- app/assets/javascripts/backstage/models/cinema.js
+++ app/assets/javascripts/backstage/models/cinema.js
@@ -1,10 +1,12 @@
 Backstage.Cinema = DS.Model.extend({
-  name:       DS.attr('string'),
-  slug:       DS.attr('string'),
-  latitude:   DS.attr('number'),
-  longitude:  DS.attr('number'),
-  status:     DS.attr('string'),
-  fetcher:    DS.attr('string'),
-  createdAt:  DS.attr('date'),
-  updatedAt:  DS.attr('date')
+  name:         DS.attr('string'),
+  slug:         DS.attr('string'),
+  latitude:     DS.attr('number'),
+  longitude:    DS.attr('number'),
+  status:       DS.attr('string'),
+  fetcher:      DS.attr('string'),
+  phoneNumber:  DS.attr('string'),
+  website:      DS.attr('string'),
+  createdAt:    DS.attr('date'),
+  updatedAt:    DS.attr('date')
 })
--- app/views/cinemas/index.json.jbuilder
+++ app/views/cinemas/index.json.jbuilder
@@ -1,5 +1,5 @@
 json.cinemas do |json|
   json.array! @cinemas do |cinema|
-    json.(cinema, :id, :name, :slug, :latitude, :longitude, :status, :fetcher, :created_at, :updated_at)
+    json.(cinema, :id, :name, :slug, :latitude, :longitude, :status, :fetcher, :phone_number, :website, :created_at, :updated_at)
   end
 end
