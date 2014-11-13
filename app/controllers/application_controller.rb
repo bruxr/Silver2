a5818de363a6d4a2fe7efbe1b994460485a57c13
brux <imoz39@gmail.com>
2014-11-14 00:02:54 +0800

    add sleep when in dev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  # Add a short 3 second sleep for simulating slow
+  # responses in the internet
+  before_action do
+    sleep 3 if Rails.env.development?
+  end
+  
   def not_found
     raise ActionController::RoutingError.new('Not Found')
   end
