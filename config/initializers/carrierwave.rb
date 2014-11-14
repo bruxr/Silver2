0e3b11203a81f2e103234d762ffb52ffbc88250b
brux <imoz39@gmail.com>
2014-11-14 10:19:51 +0800

    store bucket in fog.yml & use dash notation

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,5 +2,5 @@ CarrierWave.configure do |config|
   fog_config = YAML.load(ERB.new(File.read(Rails.root.join('config', 'fog.yml'))).result)
   fog_config = fog_config['default'].symbolize_keys
   config.fog_credentials = fog_config
-  config.fog_directory = 'silver.bruxromuar.com'
+  config.fog_directory = fog_config[:bucket]
 end
